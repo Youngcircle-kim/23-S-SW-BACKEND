9ef2fee1fa7ff07ec7524de7dd5a5446ab73b582
youngcircle-kim <dudghks021121@gmail.com>
2023-07-23 23:04:40 +0900

    feat: typeorm, nest/config 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { UsersModule } from './users/users.module';
 import { JobInfoModule } from './job-info/job-info.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { ResumesModule } from './resumes/resumes.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
 
 @Module({
-  imports: [UsersModule, JobInfoModule, AppointmentsModule, ResumesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    UsersModule,
+    JobInfoModule,
+    AppointmentsModule,
+    ResumesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
