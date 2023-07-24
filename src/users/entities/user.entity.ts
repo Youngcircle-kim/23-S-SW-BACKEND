34c8ad96b2d41a781f4b7e18d6da6ae3386f3af1
youngcircle-kim <dudghks021121@gmail.com>
2023-07-24 17:00:05 +0900

    feat: user entity 생성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class User {}
+import { IsEmail } from 'class-validator';
+import { BaseTimeEntity } from 'src/common/entities/BaseTimeEntity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseTimeEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  major: number;
+
+  @Column()
+  field: number;
+
+  @Column()
+  detailField: number;
+
+  static of(
+    email: string,
+    password: string,
+    major: number,
+    field: number,
+    detailField: number,
+  ): User {
+    const user: User = new User();
+
+    user.email = email;
+    user.password = password;
+    user.major = major;
+    user.field = field;
+    user.detailField = detailField;
+
+    return user;
+  }
+}
